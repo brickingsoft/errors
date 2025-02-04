9f8221ca866ce2bbc6c46807a976e5afea5dc047
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-04 20:34:15 +0800

    mod

---
+++ errors.go
@@ -0,0 +1,24 @@
+package errors
+
+func Join(errs ...error) error {
+	//errsLen := len(errs)
+	//if errsLen == 0 {
+	//	return nil
+	//}
+	//first := errs[0]
+	//ee, ok := first.(*EnhancedError)
+	//if !ok {
+	//	ee = New(first.Error()).(*EnhancedError)
+	//}
+	//errors.Join()
+	//for i := 1; i < errsLen; i++ {
+	//	err := errs[i]
+	//	ee.Wrapped = err
+	//
+	//}
+	//for i := errsLen-1; i < 1; i-- {
+	//	err := errs[i]
+	//
+	//}
+	return nil
+}
