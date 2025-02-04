e954d5cf2aafc5ab64b953d9a4f8cefac1d93d12
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-04 20:34:36 +0800

    mod

--- error.go
+++ error.go
@@ -3,6 +3,8 @@ package errors
 import (
 	"errors"
 	"fmt"
+	"runtime"
+	"strings"
 	"unsafe"
 )
 
@@ -12,7 +14,7 @@ func Define(message string) error {
 
 type Options struct {
 	Meta    Meta
-	Wrapped error
+	Wrapped *EnhancedError
 	Depth   int
 }
 
@@ -21,7 +23,13 @@ type Option func(*Options)
 func WithWrapped(err error) Option {
 	return func(o *Options) {
 		if err != nil {
-			o.Wrapped = err
+			if ee, ok := err.(*EnhancedError); ok {
+				o.Wrapped = ee
+			} else {
+				o.Wrapped = &EnhancedError{
+					Message: err.Error(),
+				}
+			}
 		}
 	}
 }
@@ -36,29 +44,56 @@ func New(message string, opt ...Option) error {
 	opts := Options{
 		Meta:    nil,
 		Wrapped: nil,
-		Depth:   2,
+		Depth:   1,
 	}
 	for _, o := range opt {
 		o(&opts)
 	}
 
+	st := Stacktrace{}
+	pc, file, line, ok := runtime.Caller(opts.Depth)
+	if ok {
+		if strings.IndexByte(file, '/') == 0 || strings.IndexByte(file, ':') == 1 {
+			idx := strings.Index(file, "/src/")
+			if idx > 0 {
+				file = file[idx+5:]
+			} else {
+				idx = strings.Index(file, "/pkg/mod/")
+				if idx > 0 {
+					file = file[idx+9:]
+				}
+			}
+		}
+		fn := runtime.FuncForPC(pc)
+
+		st.Fn = fn.Name()
+		st.File = file
+		st.Line = line
+	}
+
 	return &EnhancedError{
 		Message:    message,
 		Meta:       opts.Meta,
-		Stacktrace: newStacktrace(opts.Depth),
+		Stacktrace: st,
 		Wrapped:    opts.Wrapped,
 	}
+}
+
+type Stacktrace struct {
+	Fn   string
+	File string
+	Line int
 }
 
 type EnhancedError struct {
 	Message    string
 	Stacktrace Stacktrace
 	Meta       Meta
-	Wrapped    error
+	Wrapped    *EnhancedError
 }
 
 func (e *EnhancedError) Error() string {
-	return e.String()
+	return e.Message
 }
 
 func (e *EnhancedError) Unwrap() error {
@@ -78,42 +113,33 @@ func (e *EnhancedError) Is(err error) bool {
 }
 
 func (e *EnhancedError) String() string {
-	return fmt.Sprintf("%+s", e)
+	return fmt.Sprintf("%v", e)
 }
 
 func (e *EnhancedError) write(state fmt.State) {
 	buf := acquireByteBuffer()
 	_, _ = buf.WriteString("EnhancedError:\n")
-	var err error = e
+	var err = e
 WRITE:
-	switch ee := err.(type) {
-	case *EnhancedError:
-		_, _ = buf.WriteString(">>>>>>>>>>>>>\n")
-		_, _ = buf.WriteString(fmt.Sprintf("ERRO      = %s\n", ee.Message))
-		if ee.Meta.Len() > 0 {
-			_, _ = buf.WriteString("META      =")
-			for j := range ee.Meta {
-				if key, val := ee.Meta[j].Key, ee.Meta[j].Value; key != "" {
-					_, _ = buf.WriteString(fmt.Sprintf(" [%s: %v]", key, val))
-				}
+	_, _ = buf.WriteString(">>>>>>>>>>>>>\n")
+	_, _ = buf.WriteString(fmt.Sprintf("ERRO      = %s\n", err.Message))
+	if err.Meta.Len() > 0 {
+		_, _ = buf.WriteString("META      =")
+		for j := range err.Meta {
+			if key, val := err.Meta[j].Key, err.Meta[j].Value; key != "" {
+				_, _ = buf.WriteString(fmt.Sprintf(" [%s: %v]", key, val))
 			}
-			_, _ = buf.WriteString("\n")
 		}
-		if fn, file, line := ee.Stacktrace.Fn, ee.Stacktrace.File, ee.Stacktrace.Line; file != "" {
-			_, _ = buf.WriteString(fmt.Sprintf("FUNC      = %s\n", fn))
-			_, _ = buf.WriteString(fmt.Sprintf("SEEK      = %s:%d\n", file, line))
-		}
-		_, _ = buf.WriteString("<<<<<<<<<<<<<\n")
-		if ee.Wrapped != nil {
-			err = ee.Wrapped
-			goto WRITE
-		}
-		break
-	default:
-		_, _ = buf.WriteString(">>>>>>>>>>>>>\n")
-		_, _ = buf.WriteString(err.Error())
-		_, _ = buf.WriteString("\n<<<<<<<<<<<<<\n")
-		break
+		_, _ = buf.WriteString("\n")
+	}
+	if fn, file, line := err.Stacktrace.Fn, err.Stacktrace.File, err.Stacktrace.Line; file != "" {
+		_, _ = buf.WriteString(fmt.Sprintf("FUNC      = %s\n", fn))
+		_, _ = buf.WriteString(fmt.Sprintf("SEEK      = %s:%d\n", file, line))
+	}
+	_, _ = buf.WriteString("<<<<<<<<<<<<<\n")
+	if err.Wrapped != nil {
+		err = err.Wrapped
+		goto WRITE
 	}
 	b := buf.Bytes()
 	bLen := len(b)
@@ -126,14 +152,8 @@ WRITE:
 func (e *EnhancedError) Format(state fmt.State, verb rune) {
 	switch verb {
 	case 's':
-		switch {
-		case state.Flag('+'):
-			e.write(state)
-			break
-		default:
-			_, _ = fmt.Fprint(state, e.Message)
-			break
-		}
+		_, _ = fmt.Fprint(state, e.Message)
+		break
 	case 'v':
 		e.write(state)
 		break
